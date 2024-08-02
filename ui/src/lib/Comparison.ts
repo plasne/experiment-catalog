d5a21d2467a858d8bf67336cbd819c97785e0cdb
Peter Lasne <plasne@iname.com>
2024-08-02 09:25:00 -0400

    added more counts, show total experiments

--- ui/src/lib/Comparison.ts
+++ ui/src/lib/Comparison.ts
@@ -1,4 +1,5 @@
 interface Comparison {
+    total_experiment_count: number;
     last_result_for_baseline_experiment: Result;
     baseline_result_for_chosen_experiment: Result;
     last_results_for_chosen_experiment: Result[];
